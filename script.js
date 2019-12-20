7ef59975c7ed6e4b3b25b697b98b098d1c4792fa
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2019-12-20 20:05:45 +0200

    Lection done

--- script.js
+++ script.js
@@ -1,66 +1,26 @@
-// const a = 155;
-// const b = 0;
-// let result;
-
-// if ((a !== b) {
-//     alert('equal');
-// }
-
-// if (a !== 10 && a !==0 ) {
-//     result = 'result is 10';
-//     alert();
-// }else {
-//     result = ' I dont know';
-// }
-
-// result = (a !== 10 && a !==0 ) ? 'result is not 10 not 0' :'';
-
-// if (a == 1) {
-//     alert('1');
-// } else if (a == 10 || a == 20) {
-//     alert('a is 10 or 20');
-// } else if (a == 0) {
-//     alert('a is 0');
-// } else {
-//     alert('nothing');
-// }
-
-// switch (a) {
-//     case 0:
-//         alert('a is 0');
-//         break;
-//     case b:
-//         alert('1');
-//         break;
-//     case 10:
-//     case 20:
-//         alert('a is 10 or 20');
-//         break;
-//     default:
-//         alert('nothing');
-// }
-
-for (let i = 0; ; i++) {
-    alert(i);
-    if (i > 10) {
-        break;
+const constactsList = [
+    {
+        name: 'Alex',
+        surname: 'Smith'
+    },
+    {
+        name: 'Bob',
+        surname: 'Thomson'
+    },
+    {
+        name: 'Tim',
+        surname: 'Thomson'
+    },
+    {
+        name: 'Tom',
+        surname: 'Thomson'
     }
-    alert('adasd');
-}
+];
 
-// let i = 0;
-// while (i < 10) {
-//     alert(i);
-//     i++;
-// }
+constactsList.forEach(item => console.log(item));
 
-// let val;
-// do {
-//     val = +prompt('Enter number');
-// } while (isNaN(val));
+const namesList = constactsList.map(item => {
+    return item.name;
+});
 
-// let val = +prompt('Enter number');
-
-// while (isNaN(val)) {
-//     val = +prompt('Введи число');
-// }
+namesList.join();
