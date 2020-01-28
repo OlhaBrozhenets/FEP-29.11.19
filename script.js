8ad7acd8cf985cc0c14e09040f559a5b8208b1a3
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2020-01-28 13:01:14 +0200

    Lection done

--- script.js
+++ script.js
@@ -1,67 +1,7 @@
-// function Log(text) {
-//     this.text = text;
-// }
-// Log.prototype.showText = function() {
-//     console.log('cb ' + Log.format(this.text || Log.DEFAULT_TEXT));
-// };
+const acc = new Accordeon(document.getElementById('myAccordeon'));
 
-// // Log.format = function(text) {
-// //     return text.toUpperCase();
-// // };
+document
+    .getElementById('btn')
+    .addEventListener('click', () => acc.showIndex(2));
 
-// // Log.DEFAULT_TEXT = 'Default';
-
-// const logger = new Log('Hello world');
-
-// class Log1 {
-//     constructor(text) {
-//         this.text = text;
-//     }
-
-//     showText(prefix) {
-//         // console.log('cb ' + Log.format(this.text || Log.DEFAULT_TEXT));
-//         console.log(prefix + this.text);
-//     }
-// }
-
-// const logger1 = new Log1('Hello world');
-
-class Human {
-    static DEFAULT_AGE = 30;
-
-    constructor(name, age) {
-        this.name = name;
-        this.type = 'Human';
-        this.age = age || Human.DEFAULT_AGE;
-    }
-    static format(text) {
-        return text.toUpperCase();
-    }
-    run() {
-        console.log(this.name + ' is running');
-    }
-}
-
-class Student extends Human {
-    constructor(name) {
-        super(name, 18);
-
-        this.type = 'Student';
-    }
-
-    run(text) {
-        console.log(this.name + ' is running fast');
-    }
-
-    eat() {
-        console.log(this.name + 'is eating');
-    }
-
-    superRun() {
-        super.run();
-    }
-}
-
-const ham = new Hamburger(Hamburger.SMALL);
-
-ham.add(Hamburger.FILLING_KETCHUP);
+console.log(acc);
